e4d081c52bbbd05de2c4ec58cd846fbc49357740
joymajumder23 <joymajumderjishu@gmail.com>
2024-04-29 17:25:17 +0600

    All Cards added and Home Card added

--- src/components/AllCards/AllCards.jsx
+++ src/components/AllCards/AllCards.jsx
@@ -1,9 +1,12 @@
 import { SiInfracost } from "react-icons/si";
 import { TbListDetails } from "react-icons/tb";
+import { MdGroups } from "react-icons/md";
+import { FcOvertime } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const AllCards = ({allSpot}) => {
     console.log(allSpot);
-    const {image, spot, avarage, totalVisitors, travel, season} = allSpot;
+    const {_id, image, spot, avarage, totalVisitors, travel, season} = allSpot;
     return (
         <div>
             <div className="card card-side bg-base-100 shadow-xl md:flex flex-col md:flex-row gap-4 mb-4 rounded-none">
@@ -12,16 +15,16 @@ const AllCards = ({allSpot}) => {
                     <h2 className="card-title">{spot}</h2>
                     <div className="badge badge-neutral">{season}</div>
                    <div className="flex justify-between">
-                    <p><span className="font-semibold">Travel Time:</span> <span>{travel}</span></p>
+                    <p className="flex items-center gap-1"><span className="font-semibold flex items-center gap-1"><FcOvertime></FcOvertime>Travel Time:</span> <span>{travel}</span></p>
                     <p className="flex items-center"><span className="font-semibold flex items-center gap-1"><SiInfracost></SiInfracost>Avarage Cost:</span> <span>{avarage}</span>
                     </p>
                    </div>
                    <div>
-                    <p><span className="font-semibold">Total Visitors Per Year:</span> <span>{totalVisitors}</span></p>
+                    <p className="flex items-center gap-1"><span className="font-semibold flex items-center gap-1"><MdGroups></MdGroups> Total Visitors Per Year:</span> <span>{totalVisitors}</span></p>
                    </div>
 
                     <div className="card-actions justify-end">
-                        <button className="btn bg-green-800 rounded-none text-white shadow-md hover:bg-blue-600"><TbListDetails></TbListDetails>View Details</button>
+                        <Link to={`/viewDetails/${_id}`}><button className="btn bg-green-800 rounded-none text-white shadow-md hover:bg-blue-600"><TbListDetails></TbListDetails>View Details</button></Link>
                     </div>
                 </div>
             </div>
--- src/components/HomeCard/HomeCard.jsx
+++ src/components/HomeCard/HomeCard.jsx
@@ -1,19 +1,24 @@
-const HomeCard = ({allSpot}) => {
+import { TbListDetails } from "react-icons/tb";
+import { Link } from "react-router-dom";
+
+const HomeCard = ({ allSpot }) => {
     console.log(allSpot);
-    const {_id, image, spot, avarage, totalVisitors, travel, season} = allSpot;
+    const { _id, image, spot, avarage, totalVisitors, travel, season, country } = allSpot;
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure><img src={image} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">
-                        Shoes!
-                        <div className="badge badge-secondary">NEW</div>
+                        <span className="text-2xl">{spot}</span>
+                        <div className="badge badge-neutral">{season}</div>
                     </h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="flex flex-col gap-2">
+                        <div className="badge badge-outline">{country}</div>
+                        <p><span className="font-medium">Total Visitors:</span> <div className="badge badge-outline">{totalVisitors}</div></p>
+                    </div>
                     <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
+                        <Link to={`/viewDetails/${_id}`}><button className="btn bg-green-800 rounded-none text-white shadow-md hover:bg-blue-600"><TbListDetails></TbListDetails>View Details</button></Link>
                     </div>
                 </div>
             </div>
