d9b3cc480933f4e0dcf0504b3abab505f843ed5b
joymajumder23 <joymajumderjishu@gmail.com>
2024-04-30 18:19:31 +0600

    update profile added

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,24 @@
 import { useContext } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
+import toast, { Toaster } from 'react-hot-toast';
 
 const Profile = () => {
-    const { user } = useContext(AuthContext);
+    const { user, updateUser } = useContext(AuthContext);
+
+    const handleUpdate = e => {
+        const form = e.target;
+        const fullName = form.fullName.value;
+        const image = form.image.value;
+
+        updateUser(fullName, image)
+        .then(() => {
+            toast.success('Updated Successfully');
+            window.location.reload();
+        })
+        .catch(error => {
+            toast.error(error.message);
+        })
+    }
     return (
         <div className="max-w-6xl mx-auto mt-12">
             <h1 className="text-3xl font-bold text-center">Profile</h1>
@@ -17,16 +33,17 @@ const Profile = () => {
                         </div>
                         <p className="text-xl font-bold mt-3">{user?.displayName}</p>
                         </div>
-                        <form className="mt-20" action="">
-                            <p className="lg:flex items-center gap-1 w-full"><span className="font-semibold">User Name: </span><input type="text" placeholder="enter name" className="input input-bordered input-success w-full max-w-xs shadow-lg mb-4" defaultValue={user?.displayName} /></p>
+                        <form onSubmit={handleUpdate} className="mt-20" action="">
+                            <p className="text-start"><span className="font-semibold">User Name: </span><input type="text" placeholder="enter name" className="input input-bordered input-success w-full max-w-xs shadow-lg mb-4 rounded-none" name="fullName" defaultValue={user?.displayName} /></p>
                             <br />
-                            <p className="lg:flex items-center gap-1"><span className="font-semibold">Photo URL:</span> <input type="text" placeholder="enter photo URL" className="input input-bordered input-success w-full max-w-xs shadow-lg mb-4" /></p>
+                            <p className="text-start"><span className="font-semibold">Photo URL:</span> <input type="text" placeholder="enter photo URL" className="input input-bordered input-success w-full max-w-xs shadow-lg mb-4 rounded-none" name="image" /></p>
                             <br />
                             <input className="btn rounded-none bg-green-800 text-white shadow-lg" type="submit" value="Update" />
                         </form>
                     </div>
                 }
             </div>
+            <Toaster></Toaster>
         </div>
     );
 };
