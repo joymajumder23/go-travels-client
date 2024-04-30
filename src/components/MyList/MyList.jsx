2e25e16ef68610fecadad472f13807a7bb183429
joymajumder23 <joymajumderjishu@gmail.com>
2024-04-30 18:02:13 +0600

    profile data adding

--- src/components/MyList/MyList.jsx
+++ src/components/MyList/MyList.jsx
@@ -3,7 +3,7 @@ import { AuthContext } from "../../provider/AuthProvider";
 import { useLoaderData } from "react-router-dom";
 
 const MyList = () => {
-  
+
     const { user } = useContext(AuthContext);
     const [allSpots, setAllSpots] = useState([]);
 
@@ -23,13 +23,13 @@ const MyList = () => {
     return (
         <div>
             <div className="max-w-6xl mx-auto mt-12">
-            <h1 className="text-3xl font-bold text-center">My List</h1>
+                <h1 className="text-3xl font-bold text-center">My List</h1>
                 <div className="overflow-x-auto mt-8">
                     <table className="table">
                         {/* head */}
                         <thead>
-                            <tr>
-                                
+                            <tr className="text-xl">
+
                                 <th>Tourists Spot Name</th>
                                 <th>Country</th>
                                 <th>Location</th>
@@ -39,11 +39,11 @@ const MyList = () => {
                         <tbody>
                             {
                                 allSpots.map(all => <tr key={all._id} className="hover">
-                                   
+
                                     <td>{all?.spot}</td>
                                     <td>{all?.country}</td>
                                     <td>{all?.location}</td>
-                                    <td className="lg:flex gap-1"><button className="btn btn-success w-16">Update</button>
+                                    <td className="lg:flex gap-1"><button onClick={()=>document.getElementById('my_modal_5').showModal()} className="btn btn-success w-16">Update</button>
                                         <button className="btn btn-error w-16">Delete</button>
                                     </td>
                                 </tr>)
@@ -51,6 +51,18 @@ const MyList = () => {
                         </tbody>
                     </table>
                 </div>
+                <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">Hello!</h3>
+                        <p className="py-4">Press ESC key or click the button below to close</p>
+                        <div className="modal-action">
+                            <form method="dialog">
+                                {/* if there is a button in form, it will close the modal */}
+                                <button className="btn">Close</button>
+                            </form>
+                        </div>
+                    </div>
+                </dialog>
             </div>
         </div>
     );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,32 @@
+import { useContext } from "react";
+import { AuthContext } from "../../provider/AuthProvider";
+
 const Profile = () => {
+    const { user } = useContext(AuthContext);
     return (
-        <div>
-            <h1>Profile</h1>
+        <div className="max-w-6xl mx-auto mt-12">
+            <h1 className="text-3xl font-bold text-center">Profile</h1>
+            <div className="mt-8">
+                {
+                    user && <div className="text-center lg:flex items-center justify-around">
+                        <div>
+                        <div className="avatar w-60">
+                            <div className="w-full rounded-full ring ring-success ring-offset-base-100 ring-offset-2 shadow-lg">
+                                <img className="w-14" src={user?.photoURL} />
+                            </div>
+                        </div>
+                        <p className="text-xl font-bold mt-3">{user?.displayName}</p>
+                        </div>
+                        <form className="mt-20" action="">
+                            <p className="lg:flex items-center gap-1 w-full"><span className="font-semibold">User Name: </span><input type="text" placeholder="enter name" className="input input-bordered input-success w-full max-w-xs shadow-lg mb-4" defaultValue={user?.displayName} /></p>
+                            <br />
+                            <p className="lg:flex items-center gap-1"><span className="font-semibold">Photo URL:</span> <input type="text" placeholder="enter photo URL" className="input input-bordered input-success w-full max-w-xs shadow-lg mb-4" /></p>
+                            <br />
+                            <input className="btn rounded-none bg-green-800 text-white shadow-lg" type="submit" value="Update" />
+                        </form>
+                    </div>
+                }
+            </div>
         </div>
     );
 };
